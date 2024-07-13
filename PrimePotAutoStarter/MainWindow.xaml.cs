a14eb0bad0c540d58328b07c48c38b65681d11bc
PrimePot <info@primepot.org>
2024-07-14 01:03:38 +0200

    Projektdateien hinzufügen.

---
+++ PrimePotAutoStarter/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimePotAutoStarter {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Yes_Click(object sender, RoutedEventArgs e) {
+            ShortCutCreator shortCutCreator = new ShortCutCreator();
+            shortCutCreator.Create();
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void No_Click(object sender, RoutedEventArgs e) {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
---
+++ PrimePotAutoStarter/ShortCutCreator.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace PrimePotAutoStarter {
+    internal class ShortCutCreator {
+        private string autoStartFolder;
+        private string shortCutUrl;
+        private string shortCutName;
+        private string shortCutFilePath;
+        private bool shortCutExists;
+
+        public ShortCutCreator() {
+            autoStartFolder = "C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
+
+            shortCutUrl = "https://twitch.tv/primepot";
+            shortCutName = "PrimePotAutoStarter";
+            shortCutFilePath = autoStartFolder + "\\" + shortCutName + ".url";
+
+            shortCutExists = CheckIfFileExists(shortCutFilePath);
+        }
+
+        public void Create() {
+            CreateShortcut(shortCutName, shortCutUrl);
+        }
+
+        private void CreateShortcut(string name, string url) {
+            using (StreamWriter writer = new StreamWriter(autoStartFolder + "\\" + name + ".url")) {
+                writer.WriteLine("[InternetShortcut]");
+                writer.WriteLine("URL=" + url);
+                writer.Flush();
+            }
+        }
+
+        private static bool CheckIfFileExists(string name) {
+            if (!File.Exists(name)) {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
